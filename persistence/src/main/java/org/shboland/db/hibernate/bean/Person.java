2cccba6cde466930e8ee836e02bed2523377b030
sybren <sybren.boland@kvk.nl>
2017-10-20 11:31:05 +0200

    AddDocker 2

--- persistence/src/main/java/org/shboland/db/hibernate/bean/Person.java
+++ persistence/src/main/java/org/shboland/db/hibernate/bean/Person.java
@@ -10,12 +10,4 @@ public class Person {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ID")
     private Long id;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
